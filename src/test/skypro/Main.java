398dcf8015c24afadb4e4105863356036e8c0eae
Irina_F <i.i.f@mail.ru>
2022-03-04 15:44:30 +0800

    Задача 1

--- src/test/skypro/Main.java
+++ src/test/skypro/Main.java
@@ -3,6 +3,15 @@ package test.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        byte shopCount = 50;
+        short officeWorkersCount = 100;
+        int shopWorkersCount = 1000;
+        long shopItemsCount = 25000L;
+        float timeBetweenShipment1 = 3.5f;
+        double timeBetweenShipment2 = 2.3333333333;
+
+
+
+        
     }
 }
